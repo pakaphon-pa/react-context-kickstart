9e74603e83be7eb1244f1d71e85dd260bc2458f5
dackmagic115 <pakaphon23@gmail.com>
2021-07-11 22:19:01 +0700

    add to cart

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import Cart from "./components/cart/Cart";\
-import CartProvider from './store/CartProvider'
+import Cart from "./components/cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShow, setCartIsShow] = useState(false);
